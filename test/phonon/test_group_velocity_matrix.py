5c92ac39b2513086a51630817d140b79c581c514
Atsushi Togo <atz.togo@gmail.com>
2021-07-26 13:16:47 +0900

    Add group_velocity_matrix

---
+++ test/phonon/test_group_velocity_matrix.py
@@ -0,0 +1,32 @@
+import numpy as np
+from phonopy.phonon.group_velocity import GroupVelocity
+from phonopy.phonon.group_velocity_matrix import GroupVelocityMatrix
+
+def test_gvm_nacl(ph_nacl):
+    qpoints = [[0.1, 0.1, 0.1]]
+    gv = GroupVelocity(ph_nacl.dynamical_matrix,
+                       symmetry=ph_nacl.primitive_symmetry)
+    gv.run(qpoints)
+    gvm = GroupVelocityMatrix(ph_nacl.dynamical_matrix,
+                              symmetry=ph_nacl.primitive_symmetry)
+    gvm.run(qpoints)
+    gvs = []
+    for mat in gvm.group_velocity_matrices[0]:
+        gvs.append(np.diagonal(mat.real))
+    gvs = np.transpose(gvs)
+    np.testing.assert_allclose(gvs, gv.group_velocities[0], atol=1e-5)
+
+
+def test_gvm_api_nacl(ph_nacl):
+    qpoints = [[0.1, 0.1, 0.1]]
+    ph_nacl.init_group_velocity_matrix()
+    gv = GroupVelocity(ph_nacl.dynamical_matrix,
+                       symmetry=ph_nacl.primitive_symmetry)
+    gv.run(qpoints)
+    gvm = ph_nacl.group_velocity_matrix
+    gvm.run(qpoints)
+    gvs = []
+    for mat in gvm.group_velocity_matrices[0]:
+        gvs.append(np.diagonal(mat.real))
+    gvs = np.transpose(gvs)
+    np.testing.assert_allclose(gvs, gv.group_velocities[0], atol=1e-5)
